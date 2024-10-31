c1c7c4049c8aee468bc0a52c5e89726518e1fc1c
nagxsan <sanchetng@gmail.com>
2024-10-31 22:49:38 +0530

    add wallet adapter logic for requesting airdrop

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletDisconnectButton, WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "@solana/wallet-adapter-react-ui/styles.css"
+import RequestAirdrop from "./components/RequestAirdrop";
 
+export default function App() {
+  const endpoint = clusterApiUrl('devnet')
+  const wallets = []
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <WalletMultiButton />
+          <WalletDisconnectButton />
+          <RequestAirdrop />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }
-
-export default App
---
+++ src/components/RequestAirdrop.jsx
@@ -0,0 +1,35 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import { LAMPORTS_PER_SOL } from "@solana/web3.js"
+import { useState } from "react"
+
+export default function RequestAirdrop() {  
+  const wallet = useWallet()
+  const { connection } = useConnection()
+
+  const [sol, setSol] = useState('')
+
+  const requestAirdrop = () => {
+    const publicKey = wallet.publicKey
+    connection.requestAirdrop(publicKey, Number(sol) * LAMPORTS_PER_SOL)
+  }
+
+  return (
+    <div>
+      {
+        wallet.wallet
+        ?
+        <div>
+          <input
+            type="text"
+            placeholder="Enter SOL amount"
+            value={sol}
+            onChange={(e) => setSol(e.target.value)}  
+          />
+          <button onClick={requestAirdrop}>Request Airdrop</button>
+        </div>
+        :
+        ''
+      }
+    </div>
+  )
+}
